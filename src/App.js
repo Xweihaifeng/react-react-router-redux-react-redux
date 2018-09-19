87967f4fb9e07cb26cc4f10c6ae9afc7d518b1ba
Xweihaifeng <Xweihaifeng@163.com>
2018-09-19 23:26:04 +0800

    添加react新项目

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React, {
+	Component
+} from 'react';
+
+class App extends Component {
+	render() {
+		return(
+			<div>
+				To get started, edit and save to reload.
+			</div>
+		);
+	}
+}
+
+export default App;
